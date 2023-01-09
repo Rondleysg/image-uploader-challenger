2fcc9fcc80f5f8eb26c93ad5b1b0d6d69ad9a800
Rondley Gregório <80141884+Rondleysg@users.noreply.github.com>
2023-01-09 12:57:31 -0300

    first page upload image

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>
+                created by <b>Rondley</b> - devChallenges.io
+            </h2>
+        </footer>
+    );
+};
+
+export default Footer;
