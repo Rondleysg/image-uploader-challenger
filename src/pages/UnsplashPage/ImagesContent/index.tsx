a832251269d076f2578626bc792b88ba7272e1b7
Rondley Gregório <80141884+Rondleysg@users.noreply.github.com>
2023-01-26 09:43:00 -0300

    fix delete image

--- src/pages/UnsplashPage/ImagesContent/index.tsx
+++ src/pages/UnsplashPage/ImagesContent/index.tsx
@@ -24,7 +24,7 @@ const ImagesContentUnsplash = ({ images, setImages }: ImagesContentUnsplashProps
     };
 
     const deleteImage = (id: string) => {
-        const token = JSON.parse(localStorage.getItem("token")!);
+        const token = localStorage.getItem("token");
         http.request({
             url: `images/${id}`,
             method: "DELETE",
